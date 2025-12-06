4087e7700708191527d04e96a99c65d46ab9f3b2
Steven <stevenmichel502@gmail.com>
2025-12-06 12:03:40 +0100

    deleted unnessesary files

--- backend/test_rules.py
+++
@@ -1,7 +0,0 @@
-from rules_handler import load_rules, get_programs
-
-def test_rules():
-    rules = load_rules()
-    programmes = get_programs(rules)
-    assert "Angewandte_Automatisierung" in programmes
-    assert rules["Masterstudium_Berufsbegleitend"]["Allgemeine_Zugangsvoraussetzungen"]["ECTS_Bachelor"] == 210
